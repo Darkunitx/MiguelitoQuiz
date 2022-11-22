9cc836ac9f5999577c0f8331d49f1746b7593a4b
Darkunitx <darkunitx@live.com>
2022-11-21 21:56:15 -0600

    updated README and moved script.js in right folder

---
+++ README.md
@@ -0,0 +1,45 @@
+# My JavaScript Coding Quiz
+
+## Description 
+- This website quizes an individual on their javascript knowledge as well as other knowledge that they never thought would come in handy. I have created as a fun little project to test my own skills working with Methods and events.
+
+## Table of contents
+- [Description](#description)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Badges](#badges)
+
+## Usage
+- <p> Javascript coding quiz tests users on knowledge. </p>
+- <a href="https://darkunitx.github.io/MiguelitoQuiz/">Javascript Coding Quiz Live Website</a>
+- <a href="https://github.com/Darkunitx/MiguelitoQuiz">Javascript Coding Quiz Github Repo</a>
+
+![Website screenshot](./assets/images/codingquiz.jpg "Website screenshot")
+
+## Credits 
+
+- In collaboration with : 
+
+- TA Chris Stallcup
+
+- Websites/References utilized to complete this project :
+- https://codepen.io/davidcochran/pen/WbWXoa
+- https://www.guru99.com/javascript-interview-questions-answers.html
+
+
+## License
+
+- MIT License
+
+- Copyright (c) [2022] [Miguel Montenegro Javascript coding quiz]
+
+- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## Badges
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- assets/script.js
+++ assets/script.js
@@ -3,50 +3,50 @@ var mainEl = document.querySelector(".main");
 var genbutton = document.querySelector("#generate");
 genbutton.addEventListener("click", startGame);
 
-var question1 = [
-    {title:"How do you spell cat?",
-    choices: ["c-e-t","c-o-t","c-a-t","d-o-g"],
-    answer: "d-o-g"}
+var question1 = [{
+    title:"Which of the following is not a Javascript Data Type?",
+    choices: ["Number","String","Boolean","Puppies"],
+    answer: "Puppies"}
 ]
 
-var question2 = [
-    {title:"What is One plus One?",
-    choices: ["One","zero","two","eight hundred and fifty five"],
-    answer: "two"}
+var question2 = [{
+    title:"What is 'this' keyword in JavaScript??",
+    choices: ["refers to the object from where it was called.","refers to whereever we point with our finger","Not sure","refers to background colors"],
+    answer: "refers to the object from where it was called."}
 ]
 
-var question3 = [
-    {title:"can i have a dollar?",
+var question3 = [{
+    title:"What is a method?",
     choices: ["no","maybe","yes","Of course! here have two dollars instead!"],
     answer: "Of course! here have two dollars instead!"}
 ]
 
-var question4 = [
-    {title:"Coolest color in the world, Go",
+var question4 = [{
+    title:"Coolest color in the world, Go",
     choices: ["yellow","cyan","red","black"],
     answer: "cyan"}
 ]
 
-var question5 = [
-    {title:"Does pineapple go on pizza?",
+var question5 = [{
+    title:"Does pineapple go on pizza?",
     choices: ["No","No","No","No"],
     answer: "No"}
 ]
 
-var question6 = [
-    {title:"How far away is the sun?",
+var question6 = [{
+    title:"How far away is the sun?",
     choices: ["93 million miles","530 billion miles","2 yards away","not close enough"],
     answer: "93 million miles"}
 ]
 
-var question7 = [
-    {title:"Is mayonnaise an instrument?",
+var question7 = [{
+    title:"Is mayonnaise an instrument?",
     choices: ["It is whatever you want it to be","Could Be","Yes, Patrick","No, Horseradish is not an instrument either"],
     answer: "No, Horseradish is not an instrument either"}
 ]
 
-var question8 = [
-    {title:"What color is a Firetruck ?",
+var question8 = [{
+    title:"What color is a Firetruck ?",
     choices: ["Rainbow","Red","black","I give up"],
     answer: "Red"}
 ]
